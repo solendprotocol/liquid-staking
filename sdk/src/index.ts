951641045576800100b7a51048f23c038fb714d9
0xripleys <0xripleys@solend.fi>
2024-10-29 16:03:08 -0400

    support the weight hook

--- sdk/src/index.ts
+++ sdk/src/index.ts
@@ -14,12 +14,14 @@ import { LstClient } from "./functions";
 const LIQUID_STAKING_INFO = {
   id: "0xdae271405d47f04ab6c824d3b362b7375844ec987a2627845af715fdcd835795",
   type: "0xba2a31b3b21776d859c9fdfe797f52b069fe8fe0961605ab093ca4eb437d2632::ripleys::RIPLEYS",
+  weightHookId:
+    "0xf244912738939d351aa762dd98c075f873fd95f2928db5fd9e74fbb01c9a686c",
 };
 
 const RPC_URL = "https://fullnode.mainnet.sui.io";
 
 const keypair = Ed25519Keypair.fromSecretKey(
-  fromBase64(process.env.SUI_SECRET_KEY!)
+  fromBase64(process.env.SUI_SECRET_KEY!),
 );
 
 async function mint(options) {
@@ -59,7 +61,7 @@ async function redeem(options) {
   if (lstCoins.data.length > 1) {
     tx.mergeCoins(
       lstCoins.data[0].coinObjectId,
-      lstCoins.data.slice(1).map((c) => c.coinObjectId)
+      lstCoins.data.slice(1).map((c) => c.coinObjectId),
     );
   }
 
@@ -92,7 +94,7 @@ async function increaseValidatorStake(options) {
     tx,
     await lstClient.getAdminCapId(keypair.toSuiAddress()),
     options.validatorAddress,
-    options.amount
+    options.amount,
   );
 
   let txResponse = await client.signAndExecuteTransaction({
@@ -117,7 +119,7 @@ async function decreaseValidatorStake(options) {
     tx,
     await lstClient.getAdminCapId(keypair.toSuiAddress()),
     options.validatorIndex,
-    options.amount
+    options.amount,
   );
 
   let txResponse = await client.signAndExecuteTransaction({
@@ -135,6 +137,7 @@ async function decreaseValidatorStake(options) {
 
 async function updateFees(options) {
   let client = new SuiClient({ url: RPC_URL });
+  let lstClient = await LstClient.initialize(client, LIQUID_STAKING_INFO);
 
   let adminCap = (
     await client.getOwnedObjects({
@@ -146,7 +149,90 @@ async function updateFees(options) {
   ).data[0];
 
   let tx = new Transaction();
-  sdk.updateFees(tx, LIQUID_STAKING_INFO, adminCap.data.objectId, options);
+  lstClient.updateFees(tx, adminCap.data.objectId, options);
+
+  let txResponse = await client.signAndExecuteTransaction({
+    transaction: tx,
+    signer: keypair,
+    options: {
+      showEvents: true,
+      showEffects: true,
+      showObjectChanges: true,
+    },
+  });
+
+  console.log(txResponse);
+}
+
+async function initializeWeightHook(options) {
+  let client = new SuiClient({ url: RPC_URL });
+  let lstClient = await LstClient.initialize(client, LIQUID_STAKING_INFO);
+
+  let tx = new Transaction();
+  let weightHookAdminCap = lstClient.initializeWeightHook(
+    tx,
+    await lstClient.getAdminCapId(keypair.toSuiAddress()),
+  );
+  tx.transferObjects([weightHookAdminCap], keypair.toSuiAddress());
+
+  let txResponse = await client.signAndExecuteTransaction({
+    transaction: tx,
+    signer: keypair,
+    options: {
+      showEvents: true,
+      showEffects: true,
+      showObjectChanges: true,
+    },
+  });
+
+  console.log(txResponse);
+}
+
+async function setValidatorAddressesAndWeights(options) {
+  let client = new SuiClient({ url: RPC_URL });
+  let lstClient = await LstClient.initialize(client, LIQUID_STAKING_INFO);
+
+  if (options.validators.length != options.weights.length) {
+    throw new Error("Validators and weights arrays must be of the same length");
+  }
+
+  let validatorAddressesAndWeights = new Map();
+  for (let i = 0; i < options.validators.length; i++) {
+    validatorAddressesAndWeights.set(
+      options.validators[i],
+      options.weights[i] as number,
+    );
+  }
+
+  console.log(validatorAddressesAndWeights);
+
+  let tx = new Transaction();
+  lstClient.setValidatorAddressesAndWeights(
+    tx,
+    LIQUID_STAKING_INFO.weightHookId,
+    await lstClient.getWeightHookAdminCapId(keypair.toSuiAddress()),
+    validatorAddressesAndWeights,
+  );
+
+  let txResponse = await client.signAndExecuteTransaction({
+    transaction: tx,
+    signer: keypair,
+    options: {
+      showEvents: true,
+      showEffects: true,
+      showObjectChanges: true,
+    },
+  });
+
+  console.log(txResponse);
+}
+
+async function rebalance(options) {
+  let client = new SuiClient({ url: RPC_URL });
+  let lstClient = await LstClient.initialize(client, LIQUID_STAKING_INFO);
+
+  let tx = new Transaction();
+  lstClient.rebalance(tx, LIQUID_STAKING_INFO.weightHookId);
 
   let txResponse = await client.signAndExecuteTransaction({
     transaction: tx,
@@ -197,19 +283,46 @@ program
   .option("--spread-fee <SPREAD_FEE>", "Spread fee")
   .action(updateFees);
 
-
 program
   .command("fetch-state")
   .description("fetch the current state of the liquid staking pool")
   .action(async () => {
     const client = new SuiClient({ url: RPC_URL });
     try {
-      const state = await sdk.fetchLiquidStakingInfo(LIQUID_STAKING_INFO, client);
+      const state = await sdk.fetchLiquidStakingInfo(
+        LIQUID_STAKING_INFO,
+        client,
+      );
       console.log("Current Liquid Staking State:");
       console.log(JSON.stringify(state, null, 2));
     } catch (error) {
       console.error("Error fetching state:", error);
     }
   });
+
+program
+  .command("initialize-weight-hook")
+  .description("initialize weight hook")
+  .action(initializeWeightHook);
+
+function collect(pair, previous) {
+  const [key, value] = pair.split("=");
+  if (!value) {
+    throw new Error(`Invalid format for ${pair}. Use key=value format.`);
+  }
+  return { ...previous, [key]: value };
+}
+
+program
+  .command("set-validator-addresses-and-weights")
+  .description("set validator addresses and weights")
+  .option("-v, --validators <VALIDATOR_ADDRESSES...>", "Validator addresses")
+  .option("-w, --weights <WEIGHTS...>", "Weights")
+  .action(setValidatorAddressesAndWeights);
+
+program
+  .command("rebalance")
+  .description("rebalance the validator set")
+  .action(rebalance);
 
 program.parse(process.argv);
