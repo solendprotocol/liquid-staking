f5c0198b9e75881e4f7cb4e2e67e751e0c92dcb0
Nope X <nope@solend.fi>
2024-10-30 12:49:06 +0800

    deployed so updating addresses

--- sdk/src/_generated/liquid_staking/index.ts
+++ sdk/src/_generated/liquid_staking/index.ts
@@ -1,9 +1,9 @@
-export const PACKAGE_ID = "0xcfbcad8e0444ca85ad7ee28054585d12a7487bdbd83569ded988ca13441bf16e";
-export var PUBLISHED_AT = "0xcfbcad8e0444ca85ad7ee28054585d12a7487bdbd83569ded988ca13441bf16e";
+export const PACKAGE_ID = "0xb0575765166030556a6eafd3b1b970eba8183ff748860680245b9edd41c716e7";
+export var PUBLISHED_AT = "0xb0575765166030556a6eafd3b1b970eba8183ff748860680245b9edd41c716e7";
 
 export function setPublishedAt(publishedAt: string) {
   PUBLISHED_AT = publishedAt;
 }
 
 
-export const PKG_V1 = "0xcfbcad8e0444ca85ad7ee28054585d12a7487bdbd83569ded988ca13441bf16e";
+export const PKG_V1 = "0xb0575765166030556a6eafd3b1b970eba8183ff748860680245b9edd41c716e7";
