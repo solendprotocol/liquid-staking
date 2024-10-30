a6fe57450ae30ecafb2b20b7caa8af1c9d0ff06e
0xshiba1 <158560741+0xshiba1@users.noreply.github.com>
2024-10-30 13:46:19 +0800

    remove cli, sdk, package.json

--- sdk/src/_generated/liquid_staking/index.ts
+++
@@ -1,9 +0,0 @@
-export const PACKAGE_ID = "0xb0575765166030556a6eafd3b1b970eba8183ff748860680245b9edd41c716e7";
-export var PUBLISHED_AT = "0xb0575765166030556a6eafd3b1b970eba8183ff748860680245b9edd41c716e7";
-
-export function setPublishedAt(publishedAt: string) {
-  PUBLISHED_AT = publishedAt;
-}
-
-
-export const PKG_V1 = "0xb0575765166030556a6eafd3b1b970eba8183ff748860680245b9edd41c716e7";
